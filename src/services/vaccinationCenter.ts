3dce238e3e28476d54af7d84548001e0eb930670
lancerace <lancerace144@gmail.com>
2022-03-20 18:47:00 +0800

    up

--- src/services/vaccinationCenter.ts
+++ src/services/vaccinationCenter.ts
@@ -3,7 +3,8 @@ import VaccinationCenter from "../entities/vaccinationCenter";
 
 
 async function getVaccinationCenters(): Promise<VaccinationCenter[]> {
-    return getRepository(VaccinationCenter).createQueryBuilder('vaccinationcenter').getMany();
+    return getRepository(VaccinationCenter).createQueryBuilder('vaccinationcenter')
+    .orderBy('vaccinationcenter.vaccinationcenterId','ASC').getMany();
 }
 
 export default { getVaccinationCenters };
